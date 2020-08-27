27985246b01d8ce14189109eacb1cc11cad6d944
Erin Dawson <erinedawson@gmail.com>
2020-08-27 13:28:27 -0600

    first commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+add_action( 'admin_bar_menu', 'dummy_alert', 999 );
+function dummy_alert( $wp_admin_bar ) {
+  $args = array(
+  'id'    => 'dummy-alert',
+  'title' => 'Step aside, Chumley'
+  );
+
+  $wp_admin_bar->add_node( $args );
+}
+
+
+//add updater
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/user-name/repo-name/',
+	__FILE__,
+	'unique-plugin-or-theme-slug'
+);
---
+++ test_update.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin's bootstrap file to launch the plugin.
+ *
+ * @package     erinedawson
+ * @author      Erin Dawson (@edawson1980)
+ * @license     GPL3+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Testing Updates
+ * Plugin URI:  https://erinedawson.com
+ * Description: just testing something out
+ * Version:     1.0.0
+ * Author:      Erin Dawson
+ * Author URI:  https://github.com/edawson1980
+ * Text Domain:
+ * Domain Path: /languages
+ * License:     GPL3+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+
+ namespace test_update;
+
+
+
+ //  Exit if accessed directly.
+ defined( 'ABSPATH' ) || exit;
+
+ /**
+  * Gets this plugin's absolute directory path.
+  *
+  * @since  2.1.0
+  * @ignore
+  * @access private
+  *
+  * @return string
+  */
+ function _get_plugin_directory() {
+ 	return __DIR__;
+ }
+
+ /**
+  * Gets this plugin's URL.
+  *
+  * @since  2.1.0
+  * @ignore
+  * @access private
+  *
+  * @return string
+  */
+ function _get_plugin_url() {
+ 	static $plugin_url;
+
+ 	if ( empty( $plugin_url ) ) {
+ 		$plugin_url = plugins_url( null, __FILE__ );
+ 	}
+
+ 	return $plugin_url;
+ }
+
+
+
+
+
+ //pull in the stuff
+ include __DIR__ . '/functions.php';
+
+
+
+ ?>
